98c6eb9e9852dfd1a53ec5bf8127472088b057cb
Roberto Prevato <roberto.prevato@gmail.com>
2020-09-13 16:30:37 +0200

    improves project template, upgrades to 0.2.5

--- app/services.py
+++ app/services.py
@@ -1,19 +1,23 @@
 """
 Use this module to register required services.
-Services registered inside a `rodi.Container` are automatically injected into request handlers.
+Services registered inside a `rodi.Container` are automatically injected into request
+handlers.
 
 For more information and documentation, see `rodi` Wiki and examples:
     https://github.com/RobertoPrevato/rodi/wiki
     https://github.com/RobertoPrevato/rodi/wiki/Examples
 """
-from rodi import Container
-from roconfiguration import Configuration
-from core.events import ServicesRegistrationContext
 from typing import Tuple
 
+from roconfiguration import Configuration
+from rodi import Container
 
-def configure_services(configuration: Configuration) \
-        -> Tuple[Container, ServicesRegistrationContext, Configuration]:
+from core.events import ServicesRegistrationContext
+
+
+def configure_services(
+    configuration: Configuration,
+) -> Tuple[Container, ServicesRegistrationContext, Configuration]:
     container = Container()
 
     context = ServicesRegistrationContext()
