27f740b97f0aa132d81155ae19b2108813903613
Roberto Prevato <roberto.prevato@gmail.com>
2023-07-26 15:28:48 +0200

    Defaults to port 44777

--- {{cookiecutter.project_name}}/dev.py
+++ {{cookiecutter.project_name}}/dev.py
@@ -18,12 +18,12 @@ else:
 
 if __name__ == "__main__":
     os.environ["APP_ENV"] = "dev"
-    port = int(os.environ.get("APP_PORT", 8000))
+    port = int(os.environ.get("APP_PORT", 44777))
 
     console = Console()
     console.rule("[bold yellow]Running for local development", align="left")
 {%- if cookiecutter.use_openapi %}
-    console.print("[bold yellow]Visit http://localhost:8000/docs")
+    console.print(f"[bold yellow]Visit http://localhost:{port}/docs")
 {%- endif %}
 
     uvicorn.run(
